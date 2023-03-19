b2016d8f73bd0935338e7fe5451ac0ce3230a367
RaZzV2 <razzrozo10@gmail.com>
2023-03-19 20:46:29 +0200

    solved bug at email change

--- app/src/main/java/com/example/news/activities/ChangeEmailActivity.java
+++ app/src/main/java/com/example/news/activities/ChangeEmailActivity.java
@@ -3,9 +3,11 @@ package com.example.news.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -18,6 +20,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ChangeEmailActivity extends AppCompatActivity {
 
@@ -31,12 +34,15 @@ public class ChangeEmailActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         realtimeDatabaseManager = new RealtimeDatabaseManager();
-        email.requestFocus();
         realtimeDatabaseManager.getCurrentUserReference().child("email").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if(snapshot.exists()){
                     email.setText(snapshot.getValue(String.class));
+                    email.setSelection(email.getText().length());
+                    email.requestFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
                 }
             }
 
@@ -64,19 +70,23 @@ public class ChangeEmailActivity extends AppCompatActivity {
 
         done.setOnClickListener(v -> {
             realtimeDatabaseManager = new RealtimeDatabaseManager();
+            realtimeDatabaseManager.reload();
             Validator validator = new Validator();
             if (validator.isValidEmail(email.getText().toString())) {
                 realtimeDatabaseManager.getFirebaseAuth().fetchSignInMethodsForEmail(email.getText().toString()).addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
                         List<String> signInMethods = task.getResult().getSignInMethods();
                         if (signInMethods == null || signInMethods.isEmpty()) { //pun notificare
-                            realtimeDatabaseManager.getCurrentUser().updateEmail(email.getText().toString()).addOnCompleteListener(secondaryTask -> {
+                            realtimeDatabaseManager.getCurrentUser().updateEmail(email.getText().toString().trim()).addOnCompleteListener(secondaryTask -> {
                                    if(secondaryTask.isSuccessful()){
                                        realtimeDatabaseManager.getCurrentUserReference().child("email").setValue(email.getText().toString());
-                                       Toast.makeText(this, "Email has been changed successfully!", Toast.LENGTH_SHORT).show();
-                                       realtimeDatabaseManager.logOut();
                                        startActivity(new Intent(ChangeEmailActivity.this, MainActivity.class));
                                        finish();
+                                       Toast.makeText(getApplicationContext(), "Email has been changed successfully!", Toast.LENGTH_SHORT).show();
+                                       realtimeDatabaseManager.logOut();
+                                   }
+                                   else {
+                                       Toast.makeText(this, Objects.requireNonNull(secondaryTask.getException()).getMessage(), Toast.LENGTH_SHORT).show();
                                    }
                             });
                         } else {
